cb79d3576b873b47bf8fe54f3898d52b2af3d909
Parvaneh Yaghoubi <yaghoubip6@gmail.com>
2026-06-17 22:12:09 +0330

    remove static export config to fix Vercel build error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
+
